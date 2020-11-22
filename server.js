5ecc83c1af4a559dd2e889ef02060f91df5426fc
Audrey Dockendorf <Audrey@Audreys-Air-2.lan>
2020-11-22 17:31:01 -0500

    updated cors origin

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 const mongoose = require("mongoose");
 var corsOptions = {
-    origin: "http://localhost:8081"
+    origin: "http://localhost:3000"
 };  
 app.use(cors(corsOptions));
 let courses = require("./model");
